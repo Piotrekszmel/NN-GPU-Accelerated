060b055b902970220b3f5d19c5d550340fc24016
Piotrekszmel <piotr.szmelczynski@gmail.com>
2021-01-24 17:42:14 +0100

    create utils.cu and utils.cuh

---
+++ src/utils/utils.cuh
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdlib>
+
+#define gpuErrCheck(ans) {gpuAssert((ans), __FILE__, __LINE__); }
+
+void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true);
+
+#define DEBUG 0
+
+#if defined(DEBUG) && DEBUG >= 1
+    #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, ##args)
+#else
+    #define DEBUG_PRINT(fmt, args...)
+#endif
+
+float randFloat(float a, float b);
+int randInt(int a, int b);
