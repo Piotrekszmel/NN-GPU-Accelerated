dc2bc121aa9f6858ffd3284b88b17483ce008cdf
Piotrekszmel <piotr.szmelczynski@gmail.com>
2021-01-26 16:25:07 +0100

    add add kernel example

--- src/main.cu
+++ src/main.cu
@@ -13,15 +13,19 @@ int main()
         h_data2[i] = i + i;
 
 
-    Tensor tensor1(h_data, 2, 5, HOST);
-    Tensor tensor2(h_data2, 2, 5, HOST);
+    Tensor tensor1(h_data, 2, 5, HostToDevice);
+    Tensor tensor2(h_data2, 2, 5, HostToDevice);
 
-    float* fetch_data = NULL;
+    float* fetch_data = new float[10];
 
+    tensor1.add(tensor2);
     tensor1.fetchDeviceData(&fetch_data);
 
+    std::cout << "\n\n";
     for (int i = 0; i < 10; i++)
-        std::cout << fetch_data[i] << "\n";
+    {
+        std::cout << h_data[i] << " + " << h_data2[i] << " = " << fetch_data[i] << "\n";
+    }
     std::cout << std::endl;
 
 
