921c3c8377ea2117215daa9938bbdc0c8e550204
Piotrekszmel <piotr.szmelczynski@gmail.com>
2021-01-26 11:32:06 +0100

    add test example of add kernel

--- src/main.cu
+++ src/main.cu
@@ -1,7 +1,28 @@
 #include "Tensor/tensor.cuh"
+#include <iostream>
 
 int main()
 {
-    Tensor tensor1(5, 10);
-    Tensor tensor2(5);
+    
+    float* h_data = new float[10];
+    for (int i = 0; i < 10; i++)
+        h_data[i] = i;
+    
+    float* h_data2 = new float[10];
+    for (int i = 0; i < 10; i++)
+        h_data2[i] = i + i;
+
+
+    Tensor tensor1(h_data, 2, 5, HOST);
+    Tensor tensor2(h_data2, 2, 5, HOST);
+
+    float* fetch_data = NULL;
+
+    tensor1.fetchDeviceData(&fetch_data);
+
+    for (int i = 0; i < 10; i++)
+        std::cout << fetch_data[i] << "\n";
+    std::cout << std::endl;
+
+
 }
