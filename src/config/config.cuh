a6974a87555a5fbcdd35ff4a01eeddf315e3c8d6
Piotrekszmel <piotr.szmelczynski@gmail.com>
2021-01-26 16:26:13 +0100

    Create config class

---
+++ src/config/config.cuh
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+class Config
+{
+private:
+    static float strToFloat(std::string var, float defaultValue);
+    static int strToInt(std::string var, int defaultValue);
+
+public:
+    /* CUDA CONFIGURATION */
+    static int addBlockSize;
+    static int ssubtractBlockSize;
+    static int scaleBlockSize;
+    static int multiplyBlockSize;
+    static int meanBlockSize;
+    static int sharedMemory;
+    static int blockNumber;
+
+    /* NEURAL NETWORK CONFIGURATION */
+    static int epochs;
+    static int batchSize;
+    static int learningRate;
+    static int reluBlockSize;    
+    static int crossEntropyMetricBlockSize;
+    static int crossEntropyBlockSize;
+
+    static void printConfig();
+    static void printCudaConfig();
+    static void printNetConfig();
+}
