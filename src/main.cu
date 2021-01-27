fc320b567d774f3bcae5b9d50fb59d7b63810f53
Piotrekszmel <piotr.szmelczynski@gmail.com>
2021-01-27 01:49:52 +0100

    update main

--- src/main.cu
+++ src/main.cu
@@ -1,32 +1,11 @@
-#include "Tensor/tensor.cuh"
 #include <iostream>
+
+#include "Tensor/tensor.cuh"
+#include "config/config.cuh"
 
 int main()
 {
+    Config::printConfig();
     
-    float* h_data = new float[10];
-    for (int i = 0; i < 10; i++)
-        h_data[i] = i;
-    
-    float* h_data2 = new float[10];
-    for (int i = 0; i < 10; i++)
-        h_data2[i] = i + i;
-
-
-    Tensor tensor1(h_data, 2, 5, HostToDevice);
-    Tensor tensor2(h_data2, 2, 5, HostToDevice);
-
-    float* fetch_data = new float[10];
-
-    tensor1.add(tensor2);
-    tensor1.fetchDeviceData(&fetch_data);
-
-    std::cout << "\n\n";
-    for (int i = 0; i < 10; i++)
-    {
-        std::cout << h_data[i] << " + " << h_data2[i] << " = " << fetch_data[i] << "\n";
-    }
-    std::cout << std::endl;
-
-
+    return 0;
 }
