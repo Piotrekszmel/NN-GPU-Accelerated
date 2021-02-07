2db91c9811b869364c077dcf8312eca870932f6c
Piotrekszmel <piotr.szmelczynski@gmail.com>
2021-02-07 20:05:09 +0100

    Create impl for xavier initialization

--- src/utils/utils.cu
+++ src/utils/utils.cu
@@ -18,3 +18,13 @@ int randInt(int min, int max)
 {
    return min + (rand() % static_cast<int>(max - min + 1));
 }
+
+float xavierInit(int mean, int stddev, int num_input, int num_output)
+{
+   std::random_device rd;
+   std::mt19937 e2(rd());
+   std::normal_distribution<> dist(mean, stddev);
+   float rand_num = dist(e2) * sqrt(2.0f / (num_input + num_output));
+   return rand_num;
+}
+
--- src/utils/utils.cuh
+++ src/utils/utils.cuh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdio>
+#include <random>
 
 #define gpuErrCheck(ans) {gpuAssert((ans), __FILE__, __LINE__); }
 
@@ -15,4 +16,5 @@ void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true);
 #endif
 
 float randFloat(float min, float max);
+float xavierInit(int mean, int stddev, int num_input, int num_output);
 int randInt(int min, int max);
